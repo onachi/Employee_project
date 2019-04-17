8f713115d56fa4ccbdc33be472846d48ed4a86c1
59050336 <59050336@kmitl.ac.th>
2019-04-17 16:36:02 +0700

    link start end time

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,18 @@
+require 'date'
 class HomeController < ApplicationController
   def index
   end
-  def time
+  def start
+    @start_time = Time.now.to_s
+    Timesheet.update(start_time:Time.now)
+    Timesheet.update(start_date:Time.now)
+    # start time logic
+    render :index
+  end
+  def stop
+    @stop_time = Time.now.to_s
+    Timesheet.update(end_time:Time.now)
+    Timesheet.update(end_date:Time.now)
+    render :index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users , path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
-  resources :brands, only: [:index]
+  resources :home, only: [:index] do
+    collection do
+      post :start
+      post :stop
+    end
+  end
   root to: "home#index"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
