fb33d1ce423698f214143ea71b94735a4aaa135a
59050336 <59050336@kmitl.ac.th>
2019-04-23 09:41:21 +0700

    add NewTimesheet

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,16 +4,30 @@ class HomeController < ApplicationController
 
   end
   def start
+    # new timesheet
+    # User.last.timesheets << Timesheet.new
+    # add to current user
+    # current_user.timesheets << Timesheet.new(user_session)
+    #(current_user.id)
 
+    #last_timesheet  = Timesheet.find_last_by_user_id(current_user.id)
     @start_time = Time.now.to_s
     Timesheet.update(start_time:DateTime.now)
     # in_time_zone('Bangkok')
     Timesheet.update(start_date:Date.today)
+    sfgsdgdfgsdh
     # start time logic
     render :index
+
   end
+
   def stop
+
+    # find the timesheet
+
     @stop_time = Time.now.to_s
+
+    # update that one
     Timesheet.update(end_time:DateTime.now)
     Timesheet.update(end_date:Date.today)
     render :index
