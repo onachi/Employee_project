cf843c9e1ca30ab49f389ea875803a03af26ba76
59050336 <59050336@kmitl.ac.th>
2019-04-29 10:05:16 +0700

    update29Apr

--- app/controllers/timesheets_controller.rb
+++ app/controllers/timesheets_controller.rb
@@ -5,6 +5,7 @@ class TimesheetsController < ApplicationController
   end
 
   def update_status
+
      current_user.timesheets << Timesheet.new(user_session)
       timesheet =  User.find(current_user.id).timesheets.last
       timesheet.update(start_date:Date.today)
